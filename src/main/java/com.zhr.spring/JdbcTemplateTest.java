a091bb9b7bbfe7eb91df666d9a80c8b94b0806c9
zhanghaoran <2017929959@qq.com>
2022-08-20 21:45:19 +0800

    ""

--- src/main/java/com.zhr.spring/JdbcTemplateTest.java
+++ src/main/java/com.zhr.spring/JdbcTemplateTest.java
@@ -1,5 +1,6 @@
 package com.zhr.spring;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -8,11 +9,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)  // 指定当前测试类在spring的环境中执行，此时就可以通过注入的方式直接获取IOC容器中的bean、
 @ContextConfiguration("classpath:spring-jdbc.xml") // 设置spring测试环境的配置文件
-public class testJDBC {
+public class JdbcTemplateTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-
+    @Test
     public void testInsert() {
         String sql = "insert into t_user values(null,?,?,?,?,?)";
         jdbcTemplate.update(sql,"root",123,12,"女","123456@qq.com");
